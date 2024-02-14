4d0693513aee130aaa690ac7bfeb6e62dd052ed2
totti-rdz <thorsten_herfurtner@gmx.de>
2024-02-14 05:52:01 +0100

    create Sad page

---
+++ src/app/sad/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
+
+const images = [
+  "https://media.tenor.com/Q9VuGIKQqEMAAAAi/love-bear.gif",
+  "https://media.tenor.com/Pbv3D2Gk_uYAAAAi/dudu-bubu.gif",
+  "https://media.tenor.com/QOzMqPvW8PUAAAAi/love-you.gif",
+  "https://media.tenor.com/vHrITzhzd0YAAAAi/bear-panda.gif",
+  "https://media.tenor.com/RJgIui1E_2QAAAAi/teddy-bear.gif",
+];
+
+export default function Sad() {
+  const searchParams = useSearchParams();
+  const initImageIndex = () => parseInt(searchParams.get("i") || "0");
+  const [imageIndex, setImageIndex] = useState(initImageIndex);
+  const imageSrc = images[imageIndex % images.length];
+
+  const handleClick = () => setImageIndex(imageIndex + 1);
+
+  return (
+    <main
+      className="flex h-full flex-col items-center justify-between"
+      onClick={handleClick}
+    >
+      <div className="flex flex-col">
+        <div className="mb-4 h-20">
+          <h1 className="text-center text-4xl font-bold ">Okay...</h1>
+        </div>
+        <div className="grid place-content-center">
+          <Image
+            src={imageSrc}
+            alt=""
+            className="mix-blend-multiply"
+            width={400}
+            height={400}
+            priority
+          />
+        </div>
+      </div>
+    </main>
+  );
+}
